a3073d9e9d3044efef50ee01f9124590e80bbb0d
Giulolo <102964406+Giulolo@users.noreply.github.com>
2024-05-29 18:14:36 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'enigma',
+  webDir: 'www'
+};
+
+export default config;
